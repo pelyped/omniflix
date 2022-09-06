65643f8a48b54cff4d2da6a05c0ee588b47a754b
pelyped <87863504+pelyped@users.noreply.github.com>
2022-09-06 10:04:42 +0300

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -1,55 +1,25 @@
 #!/bin/bash
-if [ ! $OMNIFLIX_NODENAME ]; then
-	read -p "Введите имя ноды(1в1 как у вас назывался валидатор в Omniflix): " OMNIFLIX_NODENAME
-fi
-echo 'Ваше имя ноды: ' $OMNIFLIX_NODENAME
-sleep 1
-echo 'export OMNIFLIX_NODENAME='$OMNIFLIX_NODENAME >> $HOME/.profile
 
-
-sudo apt update
-sudo apt install curl -y
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_go.sh | bash
-
-
-source $HOME/.profile
-sleep 1
-
-git clone https://github.com/Omniflix/omniflixhub.git
-cd omniflixhub
-git checkout v0.2.1
-make install
-
-chain_id=flixnet-2
-omniflixhubd init $OMNIFLIX_NODENAME --chain-id $chain_id
-# omniflixhubd keys add $OMNIFLIX_NODENAME &>> $HOME/account.txt
-#
-# omniflixhubd add-genesis-account $OMNIFLIX_NODENAME 50000000uflix
-#
-# omniflixhubd gentx $OMNIFLIX_NODENAME 50000000uflix \
-#   --pubkey=$(omniflixhubd tendermint show-validator) \
-#   --chain-id="$chain_id" \
-#   --moniker=$OMNIFLIX_NODENAME \
-#   --details="$OMNIFLIX_NODENAME from OmniNode" \
-#   --commission-rate="0.10" \
-#   --commission-max-rate="0.20" \
-#   --commission-max-change-rate="0.01" \
-#   --min-self-delegation="1"
-
-sudo tee /etc/systemd/system/omniflixhubd.service > /dev/null <<EOF
-[Unit]
-Description=OmniFlixHub Daemon
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which omniflixhubd) start
-Restart=always
-RestartSec=3
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable omniflixhubd
-#sudo systemctl start omniflixhubd
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "Начинаем обновление репрозитория "
+echo "-----------------------------------------------------------------------------"
+cd ~/pathfinder/py
+git fetch &>/dev/null
+git checkout v0.3.3-alpha &>/dev/null
+echo "Репозиторий успешно обновлен, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
+rustup update
+cargo build --release --bin pathfinder &>/dev/null
+sleep 2
+source $HOME/.bash_profile &>/dev/null
+echo "Билд завершен успешно"
+echo "-----------------------------------------------------------------------------"
+systemctl restart starknet
+echo "Нода обновлена и запущена"
+echo "-----------------------------------------------------------------------------"
