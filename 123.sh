ec5861a45f923041ff57e34f9fd79baf856d6f35
pelyped <87863504+pelyped@users.noreply.github.com>
2024-10-15 17:43:19 +0300

    123.sh

--- 123.sh
+++ 123.sh
@@ -1,85 +1,32 @@
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_1_0
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_1_0
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_1_2
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_1_2
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_2_0
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_2_0
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_2_1
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_2_1
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_3_1
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_3_1
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_3_2
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_3_2
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_3_4
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_3_4
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_1
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_1
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_2
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_2
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_3
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_3
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_4
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_4
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_5
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_5
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_6
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_6
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_7
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_7
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_8
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_8
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_10
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_10
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_13
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_13
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_15
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_4_15
-sleep 1
-sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_5_2
-sleep 1
-sudo -u casper /etc/casper/config_from_example.sh 1_5_2
+#!/bin/bash
 
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+        echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+sudo apt update && sudo apt install jq -y
+cd $HOME
+#PULSAR_LATEST_TAG=$(curl -s https://api.github.com/repos/subspace/pulsar/tags | jq -r '.[0].name')
+#wget -O pulsar https://github.com/subspace/pulsar/releases/download/$PULSAR_LATEST_TAG/pulsar-ubuntu-x86_64-v2-$PULSAR_LATEST_TAG
+#sudo chmod +x pulsar
+#sudo mv pulsar /usr/local/bin/
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3h-2024-oct-10/subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-oct-10
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3h-2024-oct-10/subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-oct-10
+sudo chmod +x subspace-node subspace-farmer
+sudo mv subspace-node /usr/local/bin/
+sudo mv subspace-farmer /usr/local/bin/
 
-sudo logrotate -f /etc/logrotate.d/casper-node
-sudo systemctl start casper-node-launcher; sleep 2
-systemctl status casper-node-launcher
-
-echo "##########################################"
-echo "Installation finished"
-echo "Bond to the network"
-echo "##########################################"
+sudo systemctl restart subspaced
+sleep 20
+sudo systemctl restart subspaced-farmer
+echo -e '\n\e[42mYour Subspace node updated successfully\e[0m\n' && sleep 1
